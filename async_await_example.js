f018f342c9afcca02d1beb8d8f092862fb8aea64
flipeprez <flipeprez@gmail.com>
2022-10-20 12:51:34 -0300

    complete excercise

--- async_await_example.js
+++ async_await_example.js
@@ -1,26 +1,41 @@
 /* to simulete a asynchronus process, 
 i use a set interval*/ 
 function orderProduct(product){
-    return new promise((resolve, reject) => {
-        console.log (`creando orden ${product} para envio`);
-        setTimeout(() => {
-            if (product === pizza){
-                resolve('preparando pizza');
-            } else {
-                reject('solo vendemos pizza');
-            }
-        }, 2000);
-    });
-
+  return new Promise((resolve, reject) => {
+    console.log (`creando ${product} para envio`);
+    setTimeout(() => {
+      if (product === 'pizza'){
+        resolve('preparando pizza');
+        } else {
+        reject(`solo vendemos pizza, ${product} no vendemos`);
+        }
+      }, 2000);
+  });
 }
 
 function processProduct(response){
-    return new promise(resolve => {
-        console.log('procesando respuesta...');
-        console.log(`"${respuesta}"`);
-        setTimeout(() => {
-            resolve('gracias por su compra');
-        }, 4000);
-    });
+  return new Promise(resolve => {
+    console.log('procesando respuesta...');
+    console.log(`"${response}"`);
+    setTimeout(() => {
+      resolve('gracias por su compra');
+    }, 4000);
+  });
 }
 
+/*now we have 2 process simulation being asynchronous
+i am going to apply async and await, to determine the order
+that the process are going to be executed*/
+
+async function makeorder(product){
+  try{
+    const respuesta = await orderProduct(product);
+    console.log('response ok');
+    const resprocess = await processProduct(respuesta);
+    console.log(resprocess);
+  } catch (err){
+    console.log(err);
+  }
+}
+
+makeorder('habano');
