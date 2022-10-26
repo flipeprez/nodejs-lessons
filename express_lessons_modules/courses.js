b664eb38737461be4f638b97a6df1233787116ef
flipeprez <flipeprez@gmail.com>
2022-10-26 20:23:16 -0300

    add routes and other modifications to the files

--- express_lessons_modules/courses.js
+++ express_lessons_modules/courses.js
@@ -3,42 +3,58 @@ let courses = {
     programming: [
         {
             id: 1,
-            title: 'learn python',
-            language: 'python', 
+            title: "learn python",
+            language: "python", 
             views: 568974,
-            level: 'basic',
+            level: "basic",
 
         },
         {
             id: 2,
-            title: 'learn C',
-            language: 'C',
+            title: "learn C",
+            language: "C",
             views: 12354,
-            level:'medium',
+            level:"medium",
         },
         {
             id: 3,
-            title: 'javascript',
-            language: 'javascript',
+            title: "javascript",
+            language: "javascript",
             views: 65432,
-            level:'hard',
+            level:"hard",
+        },
+        {
+            id: 4,
+            title: "learn python advanced",
+            language: "python", 
+            views: 56825,
+            level: "basic",
+
         }
     ],
     maths: [
         {
             id: 1,
-            title:'maths',
-            language:'maths',
+            title:"advance maths",
+            language:"maths",
             views: 256123,
-            level: 'hard',
+            level: "hard",
 
         },
         {
             id: 2,
-            title:'algebra',
-            language:'maths',
+            title:"algebra",
+            language:"maths",
             views: 125436,
-            level: 'hard',
+            level: "medium",
+
+        },
+        {
+            id: 3,
+            title:"geometria",
+            language:"maths",
+            views: 125436,
+            level: "soft",
 
         }
     ]
--- express_lessons_modules/index.js
+++ express_lessons_modules/index.js
@@ -6,24 +6,80 @@ const app = express();
 
 const {courses} = require('./courses.js');
 
+/*routers
+i made only one, but you can do how many you need*/
+const routers = express.Router();
+app.use('/api/courses', routers);
+
 /*routing*/
 app.get('/', (req,res) => {
     res.send('my first server');
 });
 /*route for all courses*/
-app.get('/api/courses', (req, res) => {
+routers.get('/', (req, res) => {
     res.send(courses);
 });
 
-/*routes for programcion courses*/
-app.get('/api/courses/programming', (req, res) => {
+/*routes for programming courses*/
+routers.get('/programming', (req, res) => {
     res.send(JSON.stringify(courses.programming));
 }); 
 
 /*routes for maths courses*/
-app.get('/api/courses/maths', (req, res) => {
+routers.get('/maths', (req, res) => {
     res.send(JSON.stringify(courses.maths));
+}); 
+
+/* routes parameters*/
+routers.get('/programming/:language', (req,res) =>{
+    const language = req.params.language;
+    const result = courses.programming.filter(courses => courses.language === language);
+
+    if (result.length === 0) {
+        return res.status(404).send(`no se encontro el lenguaje:${language}`);
+    }
+    res.send(JSON.stringify(result));
 });
+ 
+/* routes parameters for maths*/
+routers.get('/maths/:title', (req ,res) => {
+    const title = req.params.title;
+    const result = courses.maths.filter(courses => courses.title === title);
+
+    if (result.length === 0){
+        return res.status(404).send(`no se encontro la materia ${title}`);
+    }
+    res.send(JSON.stringify(result));
+}); 
+
+/*routes with more than 1 parameter*/
+routers.get('/programming/:language/:level', (req, res) => {
+    const language = req.params.language;
+    const level = req.params.level;
+
+    const result = courses.programming.filter(courses => courses.language === language && courses.level === level);
+
+    if (result.length === 0){
+        return res.status(404).send(`no existe el curso ${language} con el nivel ${level} requerido`);
+    }
+    res.send(JSON.stringify(result));
+});
+
+/* routes parameters with querys*/
+routers.get('/programming/:language', (req,res) =>{
+    const language = req.params.language;
+    const result = courses.programming.filter(courses => courses.language === language);
+
+    if (result.length === 0) {
+        return res.status(404).send(`no se encontro el lenguaje:${language}`);
+    }
+
+    if(req.query.order === 'views'){
+        return res.send(JSON.stringify(result.sort((a , b) => b.views - a.views)));
+    }
+    res.send(JSON.stringify(result));
+});
+
 const PORT = process.env.PORT || 3000; /*this is because in the real world, 
 we don know which port is going to be asignated*/
 
